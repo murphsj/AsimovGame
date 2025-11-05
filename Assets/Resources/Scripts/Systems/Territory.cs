532fdc58a99d062759192c2d2787d0fccb3e94bb
murphsj <j.murphy.samuel@gmail.com>
2025-11-04 21:02:00 -0500

    initial commit

--- Assets/Resources/Scripts/Systems/Territory.cs
+++ Assets/Resources/Scripts/Systems/Territory.cs
@@ -1,25 +1,39 @@
 
+using System;
 using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
 
-class SectorData
-{
-    public int machines;
-    public int resistance;
-}
-
+[Serializable]
 class TerritoryData
 {
     public string name;
-    public int civMachines;
-    public int corpMachines;
-    public int govMachines;
+    public int3 machines;
+    public int3 resistance;
+}
+
+[Serializable]
+class MapData
+{
+    /// <summary>
+    /// Dictionary of territories on the map.
+    /// Key is the ID of the territory, an unsigned 8-bit int.
+    /// </summary>
+    public Dictionary<byte, TerritoryData> territories;
 }
 
 public class Territory
 {
+    private static MapData mapData;
+
     public string name;
     public List<Territory> neighbors;
     public TerritoryButton button;
+
+    public static void SetMapData(TextAsset jsonFile)
+    {
+        mapData = JsonUtility.FromJson<MapData>(jsonFile.text);
+    }
 
     public Territory(string name)
     {
@@ -31,5 +45,20 @@ public class Territory
     {
         this.name = name;
         this.neighbors = neighbors;
+    }
+
+    public static Territory FromId(byte territoryId, List<Territory> neighbors)
+    {
+        if (mapData == null) throw new InvalidOperationException(
+            "Territory FromId: tried to create a Territory from id with no map data loaded"
+        );
+
+        if (!mapData.territories.ContainsKey(territoryId)) throw new ArgumentException(
+            "Territory FromId: no map data found for territory id " + territoryId
+        );
+
+        TerritoryData data = mapData.territories[territoryId];
+
+        return new Territory(data.name);
     }
 }
